8c948e196d3530f4986a4d397e67322ec95e126f
rezainiet <masudrezaog@gmail.com>
2022-05-23 18:41:13 +0600

    hero section added in homepage and updated favicon and website title

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import Navbar from './Pages/Home/Shared/Navbar';
+import Navbar from './Pages/Shared/Navbar';
 import { Routes, Route, Link } from "react-router-dom";
 import Home from './Pages/Home/Home';
 
 function App() {
   return (
-    <div className='max-w-7xl mx-auto px-12'>
+    <div className='max-w-7xl mx-auto px-10'>
       <Navbar></Navbar>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/dashboard' element={<Home></Home>}></Route>
+        <Route path='/blogs' element={<Home></Home>}></Route>
+        <Route path='/login' element={<Home></Home>}></Route>
       </Routes>
     </div>
   );
--- src/Pages/Home/HomepageHero.js
+++ src/Pages/Home/HomepageHero.js
@@ -1,10 +1,26 @@
 import React from 'react';
 
 const HomepageHero = () => {
+    const bgImageStyle = {
+        backgroundImage:
+            "url('https://i.ibb.co/G9kXsGh/5083649.jpg')",
+        // height: '100vh',
+        // marginTop: '-70px',
+        // fontSize: '50px',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+    };
     return (
-        <div>
-            <h2>Homepage hero</h2>
-        </div>
+        <div class="hero min-h-screen" style={bgImageStyle}>
+            <div class="hero-overlay bg-opacity-60"></div>
+            <div class="hero-content text-center text-neutral-content">
+                <div class="max-w-md">
+                    <h1 class="mb-5 text-5xl font-bold">Parts Producer</h1>
+                    <p class="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <button class="btn btn-primary">Explore Us</button>
+                </div>
+            </div>
+        </div >
     );
 };
 

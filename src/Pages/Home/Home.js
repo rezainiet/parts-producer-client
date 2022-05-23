3277f0a8e46b7c3aef410613a3cd054959866bee
rezainiet <masudrezaog@gmail.com>
2022-05-23 17:32:08 +0600

    project initialized

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HomepageHero from './HomepageHero';
+
+const Home = () => {
+    return (
+        <div>
+            <HomepageHero></HomepageHero>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/Pages/Home/HomepageHero.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomepageHero = () => {
+    return (
+        <div>
+            <h2>Homepage hero</h2>
+        </div>
+    );
+};
+
+export default HomepageHero;

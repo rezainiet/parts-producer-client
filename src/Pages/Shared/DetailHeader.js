11d0faa767cc1f322183869ab18cc7a7bf582b20
rezainiet <masudrezaog@gmail.com>
2022-05-26 20:11:25 +0600

    implemented fetch url for updaing user info and loading user info

--- src/Pages/Shared/DetailHeader.js
+++ src/Pages/Shared/DetailHeader.js
@@ -5,7 +5,7 @@ const DetailHeader = ({ h1, h2 }) => {
         <div>
             <h2 className="text-xl text-primary font-semibold text-center mt-12">{h2}</h2>
             <h1 className="text-3xl text-center font-bold mb-10">{h1}</h1>
-            <div class="divider"></div>
+            <div className="divider"></div>
         </div>
     );
 };

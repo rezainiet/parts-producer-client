2128d946108fb8a19f16b2b6d059f494f1c921f8
rezainiet <masudrezaog@gmail.com>
2022-05-30 03:21:14 +0600

    changed product showing number in homepgae from parts.js

--- src/Pages/Home/Parts.js
+++ src/Pages/Home/Parts.js
@@ -16,7 +16,7 @@ const Parts = () => {
             <DetailHeader h1="Our Products" h2="What We Provide"></DetailHeader>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10'>
                 {
-                    parts.slice(0, 4).map(part => <Part key={part._id} part={part}></Part>)
+                    parts.slice(0, 6).map(part => <Part key={part._id} part={part}></Part>)
                 }
             </div>
         </div>
